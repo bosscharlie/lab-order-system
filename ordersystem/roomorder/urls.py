8517fac4d6c31aef2c9af4aab9c02636ef754472
bosscharlie <878574650@qq.com>
2021-10-12 14:22:19 +0800

    add jquery and bootstrap to project

---
+++ ordersystem/roomorder/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from django.contrib import admin
+from django.views.static import serve
+from django.conf import settings
+from . import views
+
+
+urlpatterns=[
+    #首页
+    path('',views.home),
+    #默认展示预定信息
+    path('index/',views.index),
+    #登录界面
+    path('login/',views.acc_login),
+    #处理预定请求
+    path('book/',views.book),
+    #登出
+    path('logout/',views.acc_logout),
+    #注册界面
+    path('reg/',views.reg),
+]
